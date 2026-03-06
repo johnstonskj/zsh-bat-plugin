6c1871c7dd2a6f5ee92f82790bb15269ec140bce
Simon Johnston <johnstonskj@gmail.com>
2026-03-06 07:39:26 -0800

    feeat: using new zplugins format

--- bat.plugin.zsh
+++ bat.plugin.zsh
@@ -1,123 +1,38 @@
 # -*- mode: sh; eval: (sh-set-shell "zsh") -*-
 #
-# Plugin Name: bat
-# Description: Simple environment setup for using `bat` as a cat replacement.
-# Repository: https://github.com/johnstonskj/zsh-bat-plugin
-#
-# Public variables:
-#
-# * `BAT`; plugin-defined global associative array with the following keys:
-#   * `_PLUGIN_DIR`; the directory the plugin is sourced from.
-#   * `_FUNCTIONS`; a list of all functions defined by the plugin.
-#   * `_OLD_MANPAGER`; remember previous value of MANPAGER.
-#   * `_OLD_MANROFFOPT`; remember previous value of MANROFFOPT.
+# @name bat
+# @brief Simple environment setup for using `bat` as a cat replacement.
+# @repository https://github.com/johnstonskj/zsh-bat-plugin
 #
 
 ############################################################################
-# Standard Setup Behavior
-############################################################################
-
-# See https://wiki.zshell.dev/community/zsh_plugin_standard#zero-handling
-0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
-0="${${(M)0:#/*}:-$PWD/$0}"
-
-# See https://wiki.zshell.dev/community/zsh_plugin_standard#standard-plugins-hash
-declare -gA BAT
-BAT[_PLUGIN_DIR]="${0:h}"
-BAT[_ALIASES]=""
-BAT[_FUNCTIONS]=""
-
-# Saving the current state for any modified global environment variables.
-BAT[_OLD_MANPAGER]="${MANPAGER}"
-BAT[_OLD_MANROFFOPT]="${MANROFFOPT}"
-
-############################################################################
-# Internal Support Functions
-############################################################################
+# @section Lifecycle
+# @description Plugin lifecycle functions.
+#
 
 #
-# This function will add to the `BAT[_FUNCTIONS]` list which is
-# used at unload time to `unfunction` plugin-defined functions.
+# @description
 #
-# See https://wiki.zshell.dev/community/zsh_plugin_standard#unload-function
-# See https://wiki.zshell.dev/community/zsh_plugin_standard#the-proposed-function-name-prefixes
+# Set the environment variables `MANPAGER' and `MANROFFOPT' for use with
+# bat as the pager. Also, alias `cat` to use bat also.
 #
-.bat_remember_fn() {
-    builtin emulate -L zsh
-
-    local fn_name="${1}"
-    if [[ -z "${BAT[_FUNCTIONS]}" ]]; then
-        BAT[_FUNCTIONS]="${fn_name}"
-    elif [[ ",${BAT[_FUNCTIONS]}," != *",${fn_name},"* ]]; then
-        BAT[_FUNCTIONS]="${BAT[_FUNCTIONS]},${fn_name}"
-    fi
-}
-.bat_remember_fn .bat_remember_fn
-
-.bat_define_alias() {
-    local alias_name="${1}"
-    local alias_value="${2}"
-
-    alias ${alias_name}=${alias_value}
-
-    if [[ -z "${BAT[_ALIASES]}" ]]; then
-        BAT[_ALIASES]="${alias_name}"
-    elif [[ ",${BAT[_ALIASES]}," != *",${alias_name},"* ]]; then
-        BAT[_ALIASES]="${BAT[_ALIASES]},${alias_name}"
-    fi
-}
-.bat_remember_fn .bat_define_alias
-
 bat_plugin_init() {
     builtin emulate -L zsh
 
+    @zplugins_envvar_save bat MANPAGER
     export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+
+    @zplugins_envvar_save bat MANROFFOPT
     export MANROFFOPT="-c"
 
-    .bat_define_alias cat $(which bat)
-    .bat_define_alias battail 'bat --paging=never -l log'
+    @zplugins_define_alias bat cat $(which bat)
+    @zplugins_define_alias bat battail 'bat --paging=never -l log'
 }
-.bat_remember_fn bat_plugin_init
 
-############################################################################
-# Plugin Unload Function
-############################################################################
-
-# See https://wiki.zshell.dev/community/zsh_plugin_standard#unload-function
+# @internal
 bat_plugin_unload() {
     builtin emulate -L zsh
 
-    # Remove all remembered functions.
-    local plugin_fns
-    IFS=',' read -r -A plugin_fns <<< "${BAT[_FUNCTIONS]}"
-    local fn
-    for fn in ${plugin_fns[@]}; do
-        whence -w "${fn}" &> /dev/null && unfunction "${fn}"
-    done
-    
-    # Remove all remembered aliases.
-    local aliases
-    IFS=',' read -r -A aliases <<< "${BAT[_ALIASES]}"
-    local alias
-    for alias in ${aliases[@]}; do
-        unalias "${alias}"
-    done
-
-    # Reset global environment variables .
-    export MANPAGER="${BAT[_OLD_MANPAGER]}"
-    export MANROFFOPT="${BAT[_OLD_MANROFFOPT]}"
-    
-    # Remove the global data variable.
-    unset BAT
-
-    # Remove this function.
-    unfunction bat_plugin_unload
+    @zplugins_envvar_restore bat MANPAGER
+    @zplugins_envvar_restore bat MANROFFOPT
 }
-
-############################################################################
-# Initialize Plugin
-############################################################################
-
-bat_plugin_init
-
-true
